2fdc0451bd07606557bc0732db14d0cfbd354a02
Oman24everDev <oman24ever@gmail.com>
2023-02-18 16:38:47 +0800

    create buttons for counter in components and added bootstrap cdn

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
+
 const App = () => {
+
+  const [counter, setCounter] = useState(0);
+
+
   return (
     <div className="App">
-      <h1>Hello! World</h1>
+      <button class="btn btn-danger px-3" onClick={ () => setCounter((prevCount) => prevCount -1) } > - </button>
+      <h1>{counter}</h1>
+      <button class="btn btn-primary px-3" onClick={ () => setCounter((addCount) => addCount +1) }> + </button>
     </div>
   );
 }
