ec3b9b4258b05aa4006e5903d6b01f941a83926c
Oman24everDev <oman24ever@gmail.com>
2023-02-18 17:17:32 +0800

    create reset button set to 0

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 
@@ -11,12 +11,12 @@ const App = () => {
       <div className="card text-center my-5">
         <div className="card-body">
           <h1>Simple Counter App</h1>
-           <button class="btn btn-danger px-3 mb-2" onClick={ () => setCounter((prevCount) => prevCount -1) } > - </button>
+           <button className="btn btn-danger px-3 mb-2" onClick={ () => setCounter((prevCount) => prevCount -1) } > - </button>
            <h3>{counter}</h3>
-           <button class="btn btn-primary px-3" onClick={ () => setCounter((addCount) => addCount +1) }> + </button>
-
+           <button className="btn btn-success px-3" onClick={ () => setCounter((addCount) => addCount +1) }> + </button>
+            <br></br>
+           <button className="btn btn-secondary px-5 mt-4" onClick={ () => setCounter(0)}>Reset</button>
         </div>
-
       </div>
     </div>
   );
