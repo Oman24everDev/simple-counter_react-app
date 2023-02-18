3587532e7bf8e4dbb675af24f12a87798adcffd1
Oman24everDev <oman24ever@gmail.com>
2023-02-18 17:09:53 +0800

    insert into div card the counter app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 
@@ -6,12 +6,18 @@ const App = () => {
 
   const [counter, setCounter] = useState(0);
 
-
   return (
-    <div className="App">
-      <button class="btn btn-danger px-3" onClick={ () => setCounter((prevCount) => prevCount -1) } > - </button>
-      <h1>{counter}</h1>
-      <button class="btn btn-primary px-3" onClick={ () => setCounter((addCount) => addCount +1) }> + </button>
+    <div className="container my-5">
+      <div className="card text-center my-5">
+        <div className="card-body">
+          <h1>Simple Counter App</h1>
+           <button class="btn btn-danger px-3 mb-2" onClick={ () => setCounter((prevCount) => prevCount -1) } > - </button>
+           <h3>{counter}</h3>
+           <button class="btn btn-primary px-3" onClick={ () => setCounter((addCount) => addCount +1) }> + </button>
+
+        </div>
+
+      </div>
     </div>
   );
 }
